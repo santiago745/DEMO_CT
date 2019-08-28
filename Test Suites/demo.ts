3e28e452029f8d3c99daed4c7e8b2dcd3ae5841d
80730520 <Admin@PE-HO-999.PERIFERIAGROUP.MS>
2019-08-27 22:44:04 -0500

    Primera V

---
+++ Test Suites/demo.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>demo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>a0126154-1313-445e-95e0-18d6a340fb98</testSuiteGuid>
+   <testCaseLink>
+      <guid>16befe44-070e-42ae-b382-8ca3025d339f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/demo</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
